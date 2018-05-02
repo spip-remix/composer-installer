78633d6205b481a495917f785d5393efc19cd75d
JamesRezo <james@rezo.net>
2018-05-02 16:29:29 +0200

    plugins & cms

--- src/SpipInstaller.php
+++ src/SpipInstaller.php
@@ -16,17 +16,39 @@ class SpipInstaller extends LibraryInstaller
             return './tmp/__spip_classic__';
         }
 
+        if ('spip-ecrire' === $package->getType()) {
+            return './ecrire';
+        }
+
+        if ('spip-prive' === $package->getType()) {
+            return './prive';
+        }
+
+        if ('spip-plugin' === $package->getType()) {
+            $extra = $this->composer->getPackage()->getExtra();
+            $template = isset($extra['spip']['template']) ? $extra['spip']['template'] : '';
+            $template = is_string($template) ? $template : '';
+            $extensions = isset($extra['spip']['extensions']) ? $extra['spip']['extensions'] : [];
+            $name = $package->getName();
+
+            if ($template === $name) {
+                return './squelettes-dist';
+            }
+
+            if (in_array($name, $extensions)) {
+                return './plugins-dist/' . $name;
+            }
+            
+            return './plugins/' . $name;
+        }
+
         if (in_array($package->getType(), array(
-            'spip-ecrire',
-            'spip-prive',
-            'spip-cms',
             'spip-lang',
             'spip-theme',
-            'spip-plugin'
         ))) {
             throw new \InvalidArgumentException(
                 'Unable to install this package as its type is not supported for now.'.
-                ' Wait for SPIP3.3 for using it.'
+                ' Wait for SPIP3.4+ for using it.'
             );
         }
     }
@@ -39,11 +61,10 @@ class SpipInstaller extends LibraryInstaller
         return in_array($packageType, array(
             'spip-classic', //SPIP Classic a.k.a. v3.0 to v3.2
             //Until SPIP 4.0 some packages need to be installed in some particular places
-            'spip-cms',     //SPIP CMS Component v3.4+ a.k.a. spip/cms
             'spip-ecrire',  //SPIP CMS Component a.k.a. spip/ecrire v3.3
             'spip-prive',   //SPIP CMS Component a.k.a. spip/prive v3.3
-            'spip-lang',    //SPIP CMS Component a.k.a. spip/lang v3.3
-            'spip-theme',   //SPIP CMS Component a.k.a. spip/theme v3.3
+            'spip-lang',    //SPIP CMS Component a.k.a. spip/lang v3.4
+            'spip-theme',   //SPIP CMS Component a.k.a. spip/theme v3.4
             'spip-plugin',  //an optional or required SPIP plugin or the default template
         ));
     }
