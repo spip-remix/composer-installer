4d71c45e1d2ed2c428ff0c123edb5dd862f9168a
JamesRezo <james@rezo.net>
2020-08-21 15:30:04 +0200

    Exclude files/directories from Classic copy on install

--- src/Classic.php
+++ src/Classic.php
@@ -22,8 +22,16 @@ class Classic
         $filesystem = new Filesystem();
         $rootDir = realpath($event->getComposer()->getConfig()->get('vendor-dir') . '/..');
 
+        $filesystem->remove($rootDir . '/tmp/__spip_classic__/.git');
         $filesystem->remove($rootDir . '/tmp/__spip_classic__/composer.json');
         $filesystem->remove($rootDir . '/tmp/__spip_classic__/composer.lock');
+        $filesystem->remove($rootDir . '/tmp/__spip_classic__/.gitignore');
+        $filesystem->remove($rootDir . '/tmp/__spip_classic__/.gitattributes');
+        $filesystem->remove($rootDir . '/tmp/__spip_classic__/.gitsvnextmodules');
+        $filesystem->remove($rootDir . '/tmp/__spip_classic__/IMG');
+        $filesystem->remove($rootDir . '/tmp/__spip_classic__/local');
+        $filesystem->remove($rootDir . '/tmp/__spip_classic__/tmp');
+        $filesystem->remove($rootDir . '/tmp/__spip_classic__/config');
         $filesystem->copy($rootDir . '/tmp/__spip_classic__', $rootDir);
         $filesystem->remove($rootDir . '/tmp/__spip_classic__');
     }
